491fdc413c0db9123274e54d5a8995c85c70edc5
yassu-rcj <m2207146@ritsumori.ed.jp>
2024-03-08 13:23:54 +0900

    AIすごすぎ

--- lib/MA/MA.cpp
+++ lib/MA/MA.cpp
@@ -2,38 +2,6 @@
 void MAandRingBufa::setlength(int length){
     this->length=length;
 }
-float MAandRingBufa::entervalue(float value){
-    this->count++;
-    if (times==0){
-  if(count<=20){
- record[count]=value;
-    average=sum/(count+1);
-    return average;
-    }else{
-     else if(count==21){
-        count==0;
-        average=((sum-record[0])/21)+(value/21);
-        times++;
-        return average;
-     }
-    }
-    }else{
-       if (count<=20){
-        average=((sum-record[count])/21)+(value/21);
-        return average;
-       }else {
-        count==0;
-        average=((sum-record[0])/21)+(value/21);
-        times++;
-        return average;
-       }
-    }
-}
-void MAandRingBufa::SumOfArray(){
-    for(int i=0;i<=20;i++){
-        sum=sum+record[i];
-    }
-}
 float MAandRingBufa::entervalue(float value) {
    sum -= record[count];
   record[count] = value;
@@ -42,3 +10,9 @@ float MAandRingBufa::entervalue(float value) {
     count = (count + 1) % length;
     return average;
 }
+void MAandRingBufa::SumOfArray(){
+    for(int i=0;i<=20;i++){
+        sum=sum+record[i];
+    }
+}
+
